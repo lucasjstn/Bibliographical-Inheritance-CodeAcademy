d1704c887da65863188db0c093be30acd6f6b28c
lucasjstn <lucas7justino@gmail.com>
2025-09-27 18:11:15 -0300

    Add Resource class with properties and status management

---
+++ BibliographicalInheritance/Resource.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace BiblioInheritance
+{
+    class Resource
+    {
+        public string Title { get; private set; }
+        public string Category { get; private set; }
+        public string Status { get; private set; }
+
+        public Resource(string title, string category)
+        {
+            Title = title;
+            Category = category;
+            Status = "Available";
+        }
+
+        public void UpdateStatus()
+        {
+            if (Status == "Out")
+            {
+                Status = "Available";
+            }
+
+            Status = "Out";
+        }
+    }
+}
